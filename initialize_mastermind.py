d037863b6116b2e138bda6e1e06fe84830dccc9e
gabeantonio <antoniogabe99@gmail.com>
2024-04-21 10:46:01 -0700

    add user guess input for player on the terminal

--- initialize_mastermind.py
+++ initialize_mastermind.py
@@ -4,7 +4,7 @@ class Game:
     def main():
         mastermind = Mastermind('1234')
         while mastermind.continue_game:
-            pass
-    
+            user_guess = input('Type your guess here: ')
+
     if __name__ == '__main__':
         main()
