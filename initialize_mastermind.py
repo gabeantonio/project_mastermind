2d3f5ef8c5565d51babde8418da73eddf7636f35
gabeantonio <antoniogabe99@gmail.com>
2024-04-21 10:56:06 -0700

    add logic to check user_guess

--- initialize_mastermind.py
+++ initialize_mastermind.py
@@ -2,12 +2,18 @@ from mastermind import Mastermind
 
 class Game:
     def main():
-        mastermind = Mastermind('')
+        mastermind = Mastermind('1234')
         while mastermind.continue_game:
             user_guess = input('Type your guess here: ')
             mastermind.add_guess(user_guess)
             guesses = mastermind.user_guesses()
             print('GUESSES --->', guesses)
+            # Add logic that will check to see if the most recent guess is correct or not:
+            if user_guess == mastermind.combination:
+                print('You won!')
+                break
+            else:
+                print('Try again!')
 
     if __name__ == '__main__':
         main()
