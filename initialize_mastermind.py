531ad94f4aba0978fb355332d83cd05025660425
gabeantonio <antoniogabe99@gmail.com>
2024-04-21 10:53:07 -0700

    call user_guesses method to view all past guesses

--- initialize_mastermind.py
+++ initialize_mastermind.py
@@ -2,9 +2,12 @@ from mastermind import Mastermind
 
 class Game:
     def main():
-        mastermind = Mastermind('1234')
+        mastermind = Mastermind('')
         while mastermind.continue_game:
             user_guess = input('Type your guess here: ')
+            mastermind.add_guess(user_guess)
+            guesses = mastermind.user_guesses()
+            print('GUESSES --->', guesses)
 
     if __name__ == '__main__':
         main()
