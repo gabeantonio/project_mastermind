08c1208f6aae912ed171976a9a0375d5cd8741fd
gabeantonio <antoniogabe99@gmail.com>
2024-04-21 10:41:21 -0700

    initialize continue game method in the game script

--- initialize_mastermind.py
+++ initialize_mastermind.py
@@ -3,7 +3,8 @@ from mastermind import Mastermind
 class Game:
     def main():
         mastermind = Mastermind('1234')
-        print(mastermind)
+        while mastermind.continue_game:
+            pass
     
     if __name__ == '__main__':
         main()
