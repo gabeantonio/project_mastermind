83726a014cf784ef2f5589d97197d8e0fddde134
gabeantonio <antoniogabe99@gmail.com>
2024-04-21 14:10:39 -0700

    add logic for completely incorrect guess

--- initialize_mastermind.py
+++ initialize_mastermind.py
@@ -15,20 +15,21 @@ def main():
             print(Fore.RED + f'Input is either too short or too long. Please make sure your input is {len(combination)} digits long. \n' + Fore.RESET)
             continue
 
-
         mastermind.add_guess(user_guess)
         guesses = mastermind.user_guesses()
         check = mastermind.check(user_guess)
-        feedback(guesses, check)
-    
+        if all_incorrect(check):
+            print(Fore.RED + f'Sorry, all are incorrect. \n' + Fore.RESET)
+        else:
+            feedback(guesses, check)
 
         
 
     # Add logic that will check to see if the most recent guess is correct or not:
     if mastermind.correct_guess:
-        print('You won!')
+        print(Fore.GREEN + 'You won! \n' + Fore.RESET)
     else:
-        print('You lost!')
+        print(Fore.RED + 'You lost! \n' + Fore.RESET )
 
 def generate_combination():
     api_url = f"https://www.random.org/integers/?num=4&min=0&max=7&col=1&base=10&format=plain&rnd=new"
@@ -38,6 +39,11 @@ def generate_combination():
         return combination
     else:
         print(f'Failed to get a random combination. Status code: {response.status_code}')
+
+def all_incorrect(check):
+    if check[0] == 0:
+        return True
+    
 
 def feedback(guesses, check):
     print(f'You guessed {check[0]} correct numbers in {check[1]} correct positions. You have {10 - len(guesses)} attempts remaining. Your previous guesses: {guesses} \n' )  
