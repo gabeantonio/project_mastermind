603c0e1f7754c318c90103c96bc0c446abcd9c6d
gabeantonio <antoniogabe99@gmail.com>
2024-04-22 11:40:39 -0700

    restructure code and add messages object

--- initialize_mastermind.py
+++ initialize_mastermind.py
@@ -4,19 +4,21 @@ from colorama import Fore
 import time
 import math
 
+TOTAL_TRIES = Mastermind.TOTAL_TRIES
+
 def main():
     combination = generate_combination()
     mastermind = Mastermind(combination)
-    # print(combination, '<------ COMBINATION')
+    print(combination, '<------ COMBINATION')
     player_score = 0
     start = time.time()
     while mastermind.continue_game():
         user_guess = input('\nType your guess here: ')
         if not user_guess.isnumeric():
-            print(Fore.RED + f'\nPlease enter numbers only. \n' + Fore.RESET)
+            print(Fore.RED + f'\n{MESSAGES["numbers_only"]}\n' + Fore.RESET) 
             continue
         if len(user_guess) != len(combination):
-            print(Fore.RED + f'\nInput is either too short or too long. Please make sure your input is {len(combination)} digits long. \n' + Fore.RESET)
+            print(Fore.RED + f'\n{MESSAGES["wrong_length"].format(len(combination))}\n' + Fore.RESET)  # Used message constant
             continue
 
         mastermind.add_guess(user_guess)
@@ -24,9 +26,9 @@ def main():
         check = mastermind.check(user_guess)
 
         if all_incorrect(check):
-            print(Fore.RED + f'Sorry, all are incorrect. \n' + Fore.RESET)
+            print(Fore.RED + f'{MESSAGES["all_incorrect"]}\n' + Fore.RESET)
         else:
-            feedback(guesses, check)
+            print(feedback(guesses, check))
 
     # Add logic that will check to see if the most recent guess is correct or not:
     if mastermind.correct_guess:
@@ -35,9 +37,9 @@ def main():
         player_score += math.floor((900 - duration) * 5)
         if player_score < 0:
             player_score = 0
-        print(Fore.GREEN + f'You won! Your final score: {player_score} \n' + Fore.RESET)
+        print(Fore.GREEN + f'{MESSAGES["won"].format(player_score)}\n' + Fore.RESET) 
     else:
-        print(Fore.RED + f'You lost! Your final score: {player_score} \n' + Fore.RESET )
+        print(Fore.RED + f'{MESSAGES["lost"].format(player_score)}\n' + Fore.RESET ) 
 
 def generate_combination():
     api_url = f"https://www.random.org/integers/?num=4&min=0&max=7&col=1&base=10&format=plain&rnd=new"
@@ -49,12 +51,21 @@ def generate_combination():
         print(f'Failed to get a random combination. Status code: {response.status_code}')
 
 def all_incorrect(check):
-    if check[0] == 0:
-        return True
+    return check[0] == 0
+
     
 def feedback(guesses, check):
-    print(Fore.GREEN + f'You guessed {check[0]} correct numbers in {check[1]} correct positions. You have {10 - len(guesses)} attempts remaining. Your previous guesses: {guesses} \n' + Fore.RESET)  
+    return Fore.GREEN + f'You guessed {check[0]} correct numbers in {check[1]} correct positions. ' \
+                        f'You have {TOTAL_TRIES - len(guesses)} attempts remaining. ' \
+                        f'Your previous guesses: {guesses}\n' + Fore.RESET 
 
+MESSAGES = {
+    "numbers_only": "Please enter numbers only.",
+    "wrong_length": "Input is either too short or too long. Please make sure your input is {} digits long.",
+    "all_incorrect": "Sorry, all are incorrect.",
+    "won": "You won! Your final score: {}",
+    "lost": "You lost! Your final score: {}"
+}
 
 if __name__ == '__main__':
     main()
